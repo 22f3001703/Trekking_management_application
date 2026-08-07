96102e48997754d3f998e8e1f0b2a9f7fa285eb5
Aashish kumar Jha <22f3001703@ds.study.iitm.ac.in>
2026-08-07 17:27:47 +0530

    :The databse and etc are set things are working finaaly now we are changing the game

---
+++ backend/init_db.py
@@ -0,0 +1,28 @@
+from app import app
+from database import db
+from models.models import User
+from werkzeug.security import generate_password_hash
+
+def init_database():
+    with app.app_context():
+        db.create_all()
+        
+        admin = User.query.filter_by(role='admin').first()
+        
+        if not admin:
+            print("Creating default admin user...")
+            default_admin = User(
+                name="System Admin",
+                email="admin@trekking.com",
+                password=generate_password_hash("admin123"),
+                role="admin",
+                status=1
+            )
+            db.session.add(default_admin)
+            db.session.commit()
+            print("Admin user created successfully (email: admin@trekking.com, password: admin123).")
+        else:
+            print("Admin user already exists.")
+
+if __name__ == '__main__':
+    init_database()
